00c24d1df9ece2ec0f3ec856c02f3e8e08b85f70
Виктория Кудрявцева <anikinawikki@gmail.com>
2023-06-02 15:18:13 +0300

    second

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     # path('cart/', include('cart.urls', namespace='cart')),
     path('', include('main.urls', namespace='djangosite'))
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
